aa92533b101298c36df6509ceebfc6db5fca23c8
wuyifei02 <wuyifei02@meituan.com>
2018-05-12 16:09:50 +0800

    xiaowj

--- configs/apiconfig.py
+++ configs/apiconfig.py
@@ -2,13 +2,13 @@
 # Config
 
 # API_INTERFACE = 'ehcomod'  # ehcomod <谜之屋专用> # webapi
-API_INTERFACE = 'webapi'  # ehcomod <谜之屋专用> # webapi
+API_INTERFACE = 'ehcomod'  # ehcomod <谜之屋专用> # webapi
 UPDATE_TIME = 60
 
 
 # Webapi token
-USERNAME = 'ehco'
-PORT = 2345
+USERNAME = 'wuyifei'
+PORT = 6666
 try:
     TOKEN = base64.b64encode(
         bytes('{}+{}'.format(USERNAME, PORT), 'utf8')).decode()
@@ -17,13 +17,13 @@ except:
         bytes('{}+{}'.format(USERNAME, PORT))).decode()
 
 
-WEBAPI_URL = 'http://127.0.0.1:8000/api'
+WEBAPI_URL = 'http://xiaowj.win:8000/api'
 NODE_ID = 1
 
 
 # Mysql
-MYSQL_CONFIG = 'configs/usermysql.json'
+MYSQL_CONFIG = 'configs/mysql.json'
 
 # MUJSON API
-MUAPI_CONFIG = 'usermuapi.json'
+# MUAPI_CONFIG = 'usermuapi.json'
 SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
